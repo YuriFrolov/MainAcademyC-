35baaf4c6b281e95908a7aca3e87b4c31992f6f1
Yuri <aka.kyran@gmail.com>
2024-01-11 15:52:25 +0200

    lab 4.1 init

---
+++ CSharp_Net_module1_4_1_lab/CSharp_Net_module1_4_1_lab/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Net_module1_4_1_lab
+{
+    class Customer
+    {
+        private string name;
+        // 6) declare private field name
+      
+        public Customer(string name)
+        {
+            this.name = name;
+        }
+        // 7) declare constructor to initialize name
+        public void SampleEventHandler(object sender, GoodsInfoEventArgs e)
+        {
+            Console.WriteLine(name + " got message about " + e.GoodsName);
+        }
+
+        // 8) declare method GotNewGoods with 2 parameters:
+        // 1 - object type
+        // 2 - GoodsInfoEventArgs type
+
+    }
+}
---
+++ CSharp_Net_module1_4_1_lab/CSharp_Net_module1_4_1_lab/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Net_module1_4_1_lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var shop = new OnlineShop();
+                        
+            // 9) declare object of OnlineShop 
+            var vasia = new Customer("Vasia");
+            var petia = new Customer("Petia");
+            // 10) declare several objects of Customer
+
+            // 11) subscribe method GotNewGoods() of every Customer instance 
+            // to event NewGoodsInfo of object of OnlineShop
+            shop.GoodsInfoEvent += vasia.SampleEventHandler;
+            shop.GoodsInfoEvent += petia.SampleEventHandler;
+            // 12) invoke method NewGoods() of object of OnlineShop
+            // discuss results
+            shop.NewGoods("apple");
+            Console.ReadLine();
+        }
+    }
+}
